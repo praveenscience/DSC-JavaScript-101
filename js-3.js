e2a592dee36b11234f23a68ce50379c2080a0e9d
Praveen Kumar <praveenscience@gmail.com>
2021-01-10 13:31:16 +0000

    Arrays and Objects.

---
+++ js-3.js
@@ -0,0 +1,44 @@
+/*
+  Primitive Types
+    * String
+    * Number
+    * Boolean (true or false)
+    * undefined
+  Complex Types
+    * Arrays
+    * Objects
+    * Functions
+  Complicated Type
+    * null
+*/
+
+var CommName = "DSC";
+var nFriends = 100;
+var IsActive = true;
+var Inactive = false;
+
+console.log({
+  CommName,
+  nFriends,
+  IsActive,
+  Inactive
+});
+
+let Fruits = ["Apples", "Bananas", "Cherries"];
+let Praveen = {
+  Name: {
+    First: "Praveen Kumar",
+    Last: "Purushothaman"
+  },
+  Age: 27,
+  Employed: true,
+  Student: false,
+  Address: {
+    City: "Chennai",
+    State: "Tamil Nadu",
+    Country: "India"
+  },
+  TechStacks: ["HTML", "CSS", "JavaScript"],
+  DL: undefined,
+  TaxInfo: null
+};
